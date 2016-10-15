c4922208746d938a73b3165acd934cfb78dd0b9d
Denalix <dadelgadoa@unal.edu.co>
2016-10-15 09:18:46 -0500

    Validacion
    This is a description yayy!!!

---
+++ Validacion.java
@@ -0,0 +1,21 @@
+
+package Bussines;
+
+public class Validacion {
+    
+   
+    
+    public boolean Val(double dato, boolean salir){
+        if(dato <= 0){
+            System.out.println("No se pueden insertar números negativos o cero. Por favor vuelva a introducir los datos.");
+            System.out.println("");
+            salir = false;
+        }
+        if(dato > 0){
+            salir = true;
+        }
+        
+       return salir;
+    }
+    
+}
